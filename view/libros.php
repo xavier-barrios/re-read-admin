304a2cc51915faa2f63807f2195a949c5b344c87
Danny Larrea <danny@MacBook-Air-de-Danny.local>
2020-09-24 19:33:23 +0200

    extensión ficheros

--- view/libros.php
+++ view/libros.php
@@ -18,9 +18,9 @@
 <div class="row">
   <div class="column middle">
     <div class="topnav">
-      <a href="../index.html">Re-Read</a>
-      <a href="libros.html" class="active">Libros</a>
-      <a href="ebooks.html">eBooks</a>
+      <a href="../index.php">Re-Read</a>
+      <a href="libros.php" class="active">Libros</a>
+      <a href="ebooks.php">eBooks</a>
     </div>
     <div class="textpage">
       <h3>Todos los libros tienen el mismo precio</h3>
