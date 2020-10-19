1793be974fbdef50b9bcca08e7fb680b729043d9
Danny Larrea <danny@MacBook-Air-de-Danny.local>
2020-10-19 09:11:13 +0200

    update 19/10/2020

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-header('Location: view/zone.admin.php');
+header('Location: view/login.php');
---
+++ view/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Re-Read | Login</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+<link href="../css/fontawesome-free-5.15.0-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+
+</head>
+<body>
+
+<div class="row">
+  <div class="column left">
+    <h2 style="text-align: center; margin: 20% 0 0 0;">Re-Read</h2>
+    <div class="form" style="margin:10%;">
+        <form action="../controller/loginController.php" method="POST">
+          <label for="fautor">Nombre de usuario</label>
+          <input type="text" id="email" name="email" placeholder="Usuario...">
+          
+          <label for="lname">Contraseña</label>
+          <input type="password" id="psswd" name="psswd" placeholder="Contraseña...">
+          
+          <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+  </div>
+  
+  <div class="column right" style="background-image: url('../img/re-read-tienda.jpg'); height:100vh">
+
+  </div>
+</div>
+  
+</body>
+</html>
